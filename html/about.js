def079a8018765df0f7ca52e56c5d02ebed50f36
jarvisjfreeman@hotmail.com <jarvisjfreeman@hotmail.com>
2016-03-23 00:00:02 -0500

    first commit

---
+++ html/about.js
@@ -0,0 +1,5 @@
+function test() {
+document.getElementById('mTitle').innerHTML="You Clicked ME";
+}
+//change = document.getElementById('marquee');
+//change.innerhtml = '<h1>Chicago A Beautiful city for the rich</h1> <h3>A nightmare for the poor</h3>';
---
+++ scripts/about.js
@@ -0,0 +1,34 @@
+function business() {
+  document.getElementById('mTitle').innerHTML = "Chicago Big City Salaries";
+  document.getElementById('mTitle2').innerHTML= "Cheaper than DC and Cali";
+}
+
+function culture() {
+ document.getElementById('mTitle').innerHTML = "Violence if your Poor";
+ document.getElementById('mTitle2').innerHTML= "Peace for the wealthy";
+}
+
+function environment() {
+  document.getElementById('mTitle').innerHTML = "Chicago home of smog";
+  document.getElementById('mTitle2').innerHTML= "The only place there is a whole foods in the hood";
+}
+
+function social() {
+  document.getElementById('mTitle').innerHTML = "A social paradise for those with the means";
+  document.getElementById('mTitle2').innerHTML= "Isolation for all others";
+}
+
+function civic() {
+  document.getElementById('mTitle').innerHTML = "Machine Politics";
+  document.getElementById('mTitle2').innerHTML= "First city to try and sell and senate seat";
+}
+
+function education() {
+  document.getElementById('mTitle').innerHTML = "Forget About it Valedictorians with 17's on their ACT";
+  document.getElementById('mTitle2').innerHTML= "Whitney Young and Private Schools for talented and Wealthy";
+}
+
+function health() {
+  document.getElementById('mTitle').innerHTML = "Obesity but Great Food ";
+  document.getElementById('mTitle2').innerHTML= "Food deserts in most impoverished areas";
+}
